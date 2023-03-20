8a5fc2ae6d714b2c2a27353650815cdb83db2b25
Henri ELOY <henri.eloy@edu.devinci.fr>
2023-03-20 19:29:36 +0100

    feat(new-products): insert all products

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-console, no-process-exit */
 const scraping_code= require('./eshops/dedicatedbrand');
 
+const {MongoClient} = require('mongodb');
+const MONGODB_URI = 'mongodb+srv://root:wcXPTRe4BQtX2AK@cluster0.e1lxg2t.mongodb.net/?retryWrites=true&writeConcern=majority';
+const MONGODB_DB_NAME = 'cluster0';
+
 async function sandbox (eshop) {
   try {
 
@@ -32,8 +36,84 @@ async function init(){
   fs.writeFile("result.json", finalJson, function(err) {
     if (err) {
         console.log(err);
-    }
-});
+    }});
+
+  await connectToMongo(finalProd);
+  //await query_Brand();
+  //await query_Less50();
+  //await query_SortByPrice();
+  //await query_SortByPrice();
+  await query_recentProd();
+}
+
+
+
+async function connectToMongo(finalProd) {
+
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
+
+  const collection = db.collection('products');
+  const result = await collection.insertMany(finalProd);
+
+  client.close();
+}
+
+async function query_Brand(){
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
+  const collection = db.collection('products');
+
+  const productsMontli = await collection.find({brand:"Montlimart"}).toArray();;
+  console.log("Montlimart products :" + JSON.stringify(productsMontli));
+  client.close();
+  return productsMontli;
+}
+
+async function query_Less50(){
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
+  const collection = db.collection('products');
+
+  const productsLess50 = await collection.find({price: {$lt: 50}}).toArray();
+  console.log("Products less than 50euros :" + JSON.stringify(productsLess50));
+  client.close();
+  return productsLess50;
+}
+
+async function query_SortByPrice(){
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
+  const collection = db.collection('products');
+
+  const productsSortedPrice = await collection.find().sort({price: 1}).toArray();
+  console.log("Products by price :" + JSON.stringify(productsSortedPrice));
+  client.close();
+  return productsSortedPrice;
+}
+
+async function query_SortByPrice(){
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
+  const collection = db.collection('products');
+
+  const productsSortedDate = await collection.find().sort({date: -1}).toArray();
+  console.log("Products by date :" + JSON.stringify(productsSortedDate));
+  client.close();
+  return productsSortedDate;
+}
+
+async function query_recentProd(){
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
+  const collection = db.collection('products');
+
+  const twoWeeksAgo = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
+  console.log(twoWeeksAgo);
+  const productsRecent = await collection.find({date: {$gt: twoWeeksAgo}}).toArray();
+  console.log("Products recent :" + JSON.stringify(productsRecent));
+  client.close();
+  return productsRecent;
 }
 
 
